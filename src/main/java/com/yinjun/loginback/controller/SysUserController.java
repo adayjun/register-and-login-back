8b46ff2b83352f875b5dae86a39474385f68c601
adayjun <2667205144@qq.com>
2022-09-04 18:16:45 +0800

    引入redis

--- src/main/java/com/yinjun/loginback/controller/SysUserController.java
+++ src/main/java/com/yinjun/loginback/controller/SysUserController.java
@@ -1,11 +1,15 @@
 package com.yinjun.loginback.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yinjun.loginback.entity.SysUserEntity;
 import com.yinjun.loginback.repo.UserRepository;
 import com.yinjun.loginback.req.SysUserLoginReq;
 import com.yinjun.loginback.req.SysUserSaveReq;
 import com.yinjun.loginback.resp.CommonResp;
 import com.yinjun.loginback.service.SysUserService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,30 +25,65 @@ public class SysUserController {
     private SysUserService sysUserService;
     @Resource
     private UserRepository userRepository;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    ObjectMapper objectMapper;
+
 
     @PostMapping("/sys-user/register")
-    public CommonResp<Object> register( @RequestBody SysUserSaveReq req){
-       // header("Access-Control-Allow-Origin:*");
+    public CommonResp<Object> register(@RequestBody SysUserSaveReq req) {
+        // header("Access-Control-Allow-Origin:*");
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
-        CommonResp<Object> resp=new CommonResp<>();
-        boolean res=sysUserService.register(req);
+        CommonResp<Object> resp = new CommonResp<>();
+        boolean res = sysUserService.register(req);
         resp.setSuccess(res);
         return resp;
     }
 
     @PostMapping("/sys-user/login")
-    public CommonResp<Object> login(@RequestBody SysUserLoginReq req){
-        CommonResp<Object> resp=new CommonResp<>();
-        boolean res=sysUserService.login(req);
+    public CommonResp<Object> login(@RequestBody SysUserLoginReq req) {
+        CommonResp<Object> resp = new CommonResp<>();
+        boolean res = sysUserService.login(req);
         resp.setSuccess(res);
         return resp;
     }
 
+    /**
+     * 用了 redis
+     * @return
+     */
     @PostMapping("/sys-user/ppp")
-    public CommonResp<Object> ppp(){
-        CommonResp<Object> resp=new CommonResp<>();
-        List<SysUserEntity> list=userRepository.findAll();
-        resp.setContent(list);
+    public CommonResp<Object> ppp() {
+        CommonResp<Object> resp = new CommonResp<>();
+        String toString = stringRedisTemplate.opsForValue().get("userall");
+        List<SysUserEntity> userEntityList;
+        if (toString != null) {
+            try {
+                userEntityList = objectMapper.readValue(toString, new TypeReference<List<SysUserEntity>>() {});
+                resp.setContent(userEntityList);
+                System.out.println("我是从缓存里取出来的！！！！！！！！！！！！！！！！！！");
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            return resp;
+        }
+
+        userEntityList = userRepository.findAll();
+        if (userEntityList.size() != 0) {
+            try {
+                stringRedisTemplate.opsForValue().set("userall", objectMapper.writeValueAsString(userEntityList));
+                System.out.println("我第一次被塞进缓存了啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊");
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+
+            resp.setContent(userEntityList);
+            return resp;
+
+        }
+
+        resp.setContent(null);
         return resp;
     }
 }
--- src/main/java/com/yinjun/loginback/service/impl/SysUserServiceImpl.java
+++ src/main/java/com/yinjun/loginback/service/impl/SysUserServiceImpl.java
@@ -2,6 +2,9 @@ package com.yinjun.loginback.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yinjun.loginback.entity.SysUserEntity;
 import com.yinjun.loginback.mapper.SysUserMapper;
 import com.yinjun.loginback.repo.UserRepository;
@@ -9,6 +12,7 @@ import com.yinjun.loginback.req.SysUserLoginReq;
 import com.yinjun.loginback.req.SysUserSaveReq;
 import com.yinjun.loginback.service.SysUserService;
 import com.yinjun.loginback.utils.CopyUtil;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
@@ -23,6 +27,11 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUserEntity
     private SysUserMapper sysUserMapper;
     @Resource
     private UserRepository userRepository;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private ObjectMapper objectMapper;
 
     @Override
     public boolean register(SysUserSaveReq req) {
@@ -42,12 +51,31 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUserEntity
 //        QueryWrapper<SysUserEntity> wrapper = new QueryWrapper<>();
 //        wrapper.lambda().eq(SysUserEntity::getLoginName,loginName);
 //        List<SysUserEntity> userEntityList = sysUserMapper.selectList(wrapper);
-          List<SysUserEntity> userEntityList=userRepository.findByLoginName(req.getLoginName());
-        if(CollectionUtils.isEmpty(userEntityList)){
-            return false;
-        }else {
+        List<SysUserEntity> userEntityList;
+        String toString=stringRedisTemplate.opsForValue().get(loginName);
+        if(toString!=null){
+            try {
+                userEntityList=objectMapper.readValue(toString, new TypeReference<List<SysUserEntity>>() {});
+                System.out.println("我是从缓存里取出来的！！！！！！！！！！！！！！登陆");
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
             return true;
+
         }
+
+
+         userEntityList=userRepository.findByLoginName(req.getLoginName());
+        if(userEntityList.size()!=0){
+            try {
+                stringRedisTemplate.opsForValue().set(loginName,objectMapper.writeValueAsString(userEntityList));
+                System.out.println("我第一次被塞进缓存了！！！！！！！！！！！！！！登陆");
+                return true;
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 
     //查询loginName是否被注册
